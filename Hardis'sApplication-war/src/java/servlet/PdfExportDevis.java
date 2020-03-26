41b0465f72c82ef3c5b8026c9679ccbc63f4bf8f
IMallyx <alexis.baillieu@icloud.com>
2020-03-26 16:16:49 +0100

    Modif valider/refuser contrat

---
+++ Hardis'sApplication-war/src/java/servlet/PdfExportDevis.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import com.itextpdf.io.font.FontProgram;
+import com.itextpdf.io.font.FontProgramFactory;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// Sharon a dit : essayer de mettre dans la servlet de base avec protected void do action et appeler la methode doGet
+@WebServlet(name = "PdfExportDevis", urlPatterns = {"/pdfexport"})
+public class PdfExportDevis extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String masterPath= request.getServletContext().getRealPath("/WEB-INF/DevisMaster.pdf");
+        response.setContentType("application/pdf");
+        
+        try( PdfReader reader = new PdfReader(masterPath);
+             PdfWriter writer = new PdfWriter(response.getOutputStream());
+             PdfDocument document = new PdfDocument(reader, writer)) {
+            
+            PdfPage page = document.getPage(1);
+            
+            PdfCanvas canvas = new PdfCanvas(page);
+            
+            FontProgram fontProgram = FontProgramFactory.createFont();
+            PdfFont font = PdfFontFactory.createFont(fontProgram, "utf-8", true);
+            canvas.setFontAndSize(font, 12);
+            
+            canvas.beginText();
+            
+            canvas.setTextMatrix(0, 0);
+            canvas.showText("Origine");
+            
+            
+            canvas.endText();
+            
+            
+        }
+                
+        
+        
+        
+        //super.doGet(request, response); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+ 
+    
+}
